5609ea0f0bc2d218172b98ec2dd4bf93c2445645
Dukjoo <dukson@gmail.com>
2016-08-24 14:23:51 +0900

    add readme1 to Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -5,7 +5,7 @@ public class Calc{
 	public int sub(int x, int y) { //add method
 	                return x-y;
 	}		
-	public int mul(int x, int y) { //add method
+	public int mul(int x, int y) { //add method, readme 1
 			return x*y;
 	}
 
