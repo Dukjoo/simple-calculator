ef50fa7c0f43c5126d137b8a6361b6b44f5fd448
Dukjoo <dukson@gmail.com>
2016-08-24 15:41:46 +0900

    Update Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,4 +1,5 @@
 public class Calc{
+	int r = 0; //
 	public int add(int x, int y) { //add method
 		return x+y;
 	} 
@@ -8,6 +9,8 @@ public class Calc{
 	public int mul(int x, int y) { //mul method, readme 1, 2
 			return x*y;
 	}
-
+	public void dec(int x) {
+		r = r-x;; //
+	}
 
 }
