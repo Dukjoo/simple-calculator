95d0935cdb34034c0ce381e554d4cdf4a0e8cf53
Dukjoo <dukson@gmail.com>
2016-08-24 14:26:16 +0900

    update mul comment 2

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -5,7 +5,7 @@ public class Calc{
 	public int sub(int x, int y) { //add method
 	                return x-y;
 	}		
-	public int mul(int x, int y) { //add method
+	public int mul(int x, int y) { //add method, mul method 2
 			return x*y;
 	}
 
