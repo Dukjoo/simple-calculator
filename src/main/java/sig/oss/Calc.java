506d6fe3e5e3551845620beaa9f8724e6ef0bbd9
Dukjoo <dukson@gmail.com>
2016-08-24 13:33:51 +0900

    add sub fuction

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,5 +1,9 @@
 public class Calc{
 	public int add(int x, int y) { //add method
 		return x+y;
-	}
+	} 
+	public int sub(int x, int y) { //add method
+	                return x-y;
+			        }
+
 }
